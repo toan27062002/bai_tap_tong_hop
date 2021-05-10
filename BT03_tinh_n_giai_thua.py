5e3a73243b0efde8ea5d88628bc432aa3ac038d0
toan27062002 <72190979+toan27062002@users.noreply.github.com>
2021-05-10 15:15:28 +0700

    Add files via upload

---
+++ BT03_tinh_n_giai_thua.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created 
+
+@author: acer
+"""
+
+n = int(input("Nhập số cần tính giai thừa: "))
+ 
+def giaiThua(n):
+    if n == 0:
+        return 1
+    return n * giaiThua(n - 1)
+ 
+print (giaiThua(n))
