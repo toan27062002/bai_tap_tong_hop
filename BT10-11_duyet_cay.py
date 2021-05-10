6a6a12003081c8048f6465e22438ec1e58299af8
toan27062002 <72190979+toan27062002@users.noreply.github.com>
2021-05-10 15:23:18 +0700

    Update BT10-11_duyet_cay.py

--- BT10-11_duyet_cay.py
+++ BT10-11_duyet_cay.py
@@ -9,5 +9,7 @@ from binarytree import build
 values = [9, 4, 2, 6, 7, None, 1, 5, 8]
 root = build(values)
 print(root)
-print(root.preorder) # duyệt cây theo thứ tự trước
-print(root.postorder) # duyệt cây theo thứ tự sau
+# duyệt cây theo thứ tự trước
+print(root.preorder)
+# duyệt cây theo thứ tự sau 
+print(root.postorder)
